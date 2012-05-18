375a5fa541675a1f78dd6a58b92f239572cde5e6
Jon Code <programcodex@gmail.com>
2012-05-18 19:10:59 -0400

    checkout scaffold and one_page HTML

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423204536) do
+ActiveRecord::Schema.define(:version => 20120518215642) do
 
   create_table "artists", :force => true do |t|
     t.string  "name"
@@ -60,6 +60,33 @@ ActiveRecord::Schema.define(:version => 20120423204536) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "customer_id"
+  end
+
+  create_table "checkouts", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "company"
+    t.text     "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country",       :default => "US"
+    t.string   "zip_code"
+    t.text     "address_ship"
+    t.string   "city_ship"
+    t.string   "state_ship"
+    t.string   "country_ship",  :default => "US"
+    t.string   "zip_code_ship"
+    t.string   "delivery_type"
+    t.datetime "ship_date"
+    t.integer  "customer_id"
+    t.integer  "artist_id"
+    t.integer  "product_id"
+    t.text     "note"
+    t.string   "pay_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "customers", :force => true do |t|
