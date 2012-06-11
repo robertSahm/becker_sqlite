2857ad4d572f34005e58007bfe0234de5b730c34
Robert Sahm <robertsahm@gmail.com>
2012-06-11 13:47:26 -0700

    cleaned up branching mistakes and javascript files

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -8,25 +8,11 @@ var shopImage = $('img.shop');
 var header = $(".headerBackground");
 */
 
-/*
-$(document).ready(function() {
-  //call image rotator
-	$('.spinner').roundabout({
-		//shape: 'tearDrop',
-		//duration: 1000,
-		minScale: .4, 
-		maxScale: 1,
-		focusBearing: 0,
-		tilt: 3,
-		minOpacity: .4,
-		minZ: 279,
-		maxZ: 280,
-		easing: "swing",
 
-	});
+$(document).ready(function() {
 
 	//Call parallax scrolling ability on elements
-	/*
+	
 	$('.headerImage').scrollingParallax({
 	  reverseDirection : true,
 	  staticSpeed: .2,
@@ -39,245 +25,29 @@ $(document).ready(function() {
 		staticSpeed: .06,
 		staticScrollLimit : true
 	});
-		$('#parallax2').scrollingParallax({
+	$('#parallax2').scrollingParallax({
 		reverseDirection : false,
 		staticSpeed: .05,
 		staticScrollLimit : true
 	});
-		$('#parallax3').scrollingParallax({
+	$('#parallax3').scrollingParallax({
 		reverseDirection : false,
 		staticSpeed: .04,
 		staticScrollLimit : true
 	});
-		$('.banjoIndex').scrollingParallax({
+	$('.banjoIndex').scrollingParallax({
 		reverseDirection : false,
 		staticSpeed: 0.6,
 		staticScrollLimit : false
 	});
-	*/
+});	
 
-	/*
-	$('.thankYou').scrollingParallax({
-		reverseDirection : true,
-		staticSpeed: 0.3,
-		staticScrollLimit : true
-	});
-	*/
-	
-	//Store Animations
-	//$(".showcaseGuitar, .showcaseBass, .showcaseCustom, .showcaseTheme").hide();
 
 
-/*
-	$('.bassClick1 a').click(function() {
-		$('li.guitarClick1 a, li.customClick1 a, li.allClick1 a').toggle(500);
-		//$('.showcaseGuitar').delay(500).toggle(500);
-		//$('.bassClick1').animate({
-		//	fontSize: ".8em",
-		//	marginTop: "-40px"
-		//}, 500 );
-		return false;
-	});
 
-	$(".scrollableArea").smoothDivScroll({ 
-		mousewheelScrolling: true,
-		manualContinuousScrolling: false,
-		visibleHotSpotBackgrounds: "always",
-		});
-});
 
-//$('.bassClick1 a').click(function() {
-//	$('h2.chooseInstrument').fadeOut() });
 
-$('.roundabout-moveable-item a').click(function() {
-	$('.showcaseGuitar h2').animate({
-			fontSize: "1.7em",
-			marginTop: "5px"
-		}, 500 );
-	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-
-
-//Store
-
-
-
-/*
- //End All
-
-
-var scroller = new StickyScroller("#frame3 h3",
-{
-    start: 2530,
-    end: 9700,
-    interval: 900,
-    range: 100,
-    margin: 1
-});
-
-
-var scroller = new StickyScroller(".youtubeContainer",
-{
-	start: 3080,
-    end: 7300,
-    interval: 900,
-    range: 900,
-    margin: 153
-});
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-	//call opacity animation 
-  var fadeStart=1000 // XXpx scroll or less will equiv to 1 opacity
-  ,fadeUntil=7000 // XXpx scroll or more will equiv to 0 opacity
-  ,fading = $('#frame2');
-		
-
-	$(window).bind('scroll', function(){
-			var offset = $(document).scrollTop()
-					,opacity=0
-			;
-			if( offset<=fadeStart ){
-					opacity=1;
-			}else if( offset<=fadeUntil ){
-					opacity=1-offset/fadeUntil;
-			}
-			fading.css('opacity',opacity).html(opacity);
-	});
-
-	//call opacity animation 
-  var fadeStart=300 // 100px scroll or less will equiv to 1 opacity
-  ,fadeUntil=1700 // 200px scroll or more will equiv to 0 opacity
-  ,fading = $('');
-		
-
-	$(window).bind('scroll', function(){
-			var offset = $(document).scrollTop()
-					,opacity=0
-			;
-			if( offset<=fadeStart ){
-					opacity=1;
-			}else if( offset<=fadeUntil ){
-					opacity=1-offset/fadeUntil;
-			}
-			fading.css('opacity',opacity).html(opacity);
-	});
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-	//Call Scrollorama
-   var scrollorama = $.scrollorama({
-       blocks:'.scrollblock',
-			 
-   });
-
-		scrollorama.animate('#parallax1',{ 
-			duration: 900, 
-			property:'margin-left',
-			start:10, 
-			end: 300 
-		})	
-		scrollorama.animate('#parallax2',{ 
-
-			duration: 900, 
-			property:'top', 
-			start:170, 
-			end: 200 
-		})
-			scrollorama.animate('#parallax3',{ 
-			duration: 700, 
-			property:'top', 
-			start:200, 
-			end:700 
-		})
-
-		scrollorama.animate('#fade-in',{
-    duration:400, 
-		property:'opacity'
-		end: 
-		})
-		
-		
-
-		
-
-
-	
-});
-
-	*/
-
-/*	
-	//Animate the Nav Bar
-	$('.navLi a').hover(
-			function(){
-			$(this).stop(true, true).animate({left: '-=50'}, 400);
-      }, 
-      function() {
-       $(this).stop(true, true).animate({left: '+=50'}, 400);
-      });
-});
-
-			
-			
-			*/
-			
 			
 			
 			
